1ffe2a6c434e4bffe3b1db5c1188036792927172
mosuromayowa <mosuromayowa@gmail.com>
2023-11-13 15:00:19 -0500

    Created the respective fields and constructor (name, age, birthyear, rate)

--- Employee.java
+++ Employee.java
@@ -1,4 +1,18 @@
 public class Employee {
+private String name;
+private int age;
+private int birthYear;
+
+private int rate;
+
+public Employee (String name, int age, int birthYear, int rate) {
+    this.age = age;
+    this.name = name;
+    this.birthYear = birthYear;
+    this.rate = rate;
+}
+
+
 
 
 
