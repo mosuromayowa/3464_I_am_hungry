922e91c6fc985fb1212e699d1f0c5875abbc26d6
mosuromayowa <mosuromayowa@gmail.com>
2023-11-13 22:33:06 -0500

    adding an overloaded method for manager, tester and programmer

--- Employee.java
+++ Employee.java
@@ -1,15 +1,18 @@
 public class Employee {
-private String name;
+
+    private int birthYear;
+    private String name;
 private int age;
-private int birthYear;
+
 
 private int rate;
 
-public Employee (String name, int age, int birthYear, int rate) {
+public Employee (String name,  int birthYear, int age, int rate) {
     this.age = age;
     this.name = name;
     this.birthYear = birthYear;
     this.rate = rate;
+
 }
 
 
--- Manager.java
+++ Manager.java
@@ -1,3 +1,13 @@
 public class Manager extends Employee {
 
+    private int nbTravelDays;
+
+    private int nbClients;
+
+    public Manager (String name,  int birthYear, int age, int rate, int nbClients, int nbTravelDays){
+        super(name, birthYear, age, rate);
+        this.nbTravelDays = nbTravelDays;
+        this.nbClients = nbClients;
+    }
+
 }
